e758c67de69f12009225fc3b5141cc6cbc3b6265
Sh4no0b <thibod@handigital.fr>
2019-01-02 17:02:00 +0100

    fin seance mercredi 2/01/2019

---
+++ view/header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../assets/css/header.css">
+
+<div class="header">
+    <img src="../assets/images/logo_minimo.png" alt="logo minimo" />
+    <ul class="menu">
+        <li><a href="#">lifestyle</a></li>
+        <li><a href="#">photodiary</a></li>
+        <li><a href="#">music</a></li>
+        <li><a href="#">travel</a></li>
+    </ul>
+</div>
