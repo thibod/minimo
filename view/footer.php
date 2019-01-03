a99c525dd2c723d48a47b84f99f44f67310b9357
Sh4no0b <thibod@handigital.fr>
2019-01-03 16:32:26 +0100

    fin de seance jeudi 3

---
+++ view/footer.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="../assets/css/foundation.css">
+<link rel="stylesheet" href="../assets/css/footer.css">
+
+
+<div class="footer">
+    <ul class="menu align-left">
+        <li>Terms & conditions</li>
+        <li>Contact</li>
+    </ul>
+
+    <ul class="menu align-right">
+        <li>Follow</li>
+        <li><img src="" alt="picto fb" /></li>
+        <li><img src="" alt="picto twitter" /></li>
+        <li><img src="" alt="picto insta" /></li>
+    </ul>
+</div>
--- view/header.php
+++ view/header.php
@@ -1,11 +1,16 @@
+<link rel="stylesheet" href="../assets/css/foundation.css">
 <link rel="stylesheet" href="../assets/css/header.css">
 
 <div class="header">
-    <img src="../assets/images/logo_minimo.png" alt="logo minimo" />
-    <ul class="menu">
-        <li><a href="#">lifestyle</a></li>
-        <li><a href="#">photodiary</a></li>
-        <li><a href="#">music</a></li>
-        <li><a href="#">travel</a></li>
-    </ul>
+    <div class="logo">
+        <img src="../assets/images/logo_minimo.png" />
+    </div>
+    <div>
+        <ul class="menu align-right">
+            <li><a href"#">lifestyle</a></li>
+            <li><a href"#">photodiary</a></li>
+            <li><a href"#">music</a></li>
+            <li><a href"#">travel</a></li>
+        </ul>
+    </div>
 </div>
